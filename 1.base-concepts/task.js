2da7602764f7e88655884260d2d36e545a763461
Andrey Chernikov <93872085+luckyPunch31@users.noreply.github.com>
2025-01-27 16:39:43 +0300

    Update task.js
    Выполнил первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let des;
+	des = (b ** 2) - 4 * a * c;
+	if (des === 0) {
+		let x1;
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	} else if (des > 0) {
+		let x1, x2;
+		x1 = (-b + Math.sqrt(des)) / (2 * a);
+		x2 = (-b - Math.sqrt(des)) / (2 * a);
+		arr.push(x1, x2);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
